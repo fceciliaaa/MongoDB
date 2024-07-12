7894111f7d8c572b3f7a8a5a5aabe59a6e14f0d8
Cecilia Ferreira <166570082+fceciliaaa@users.noreply.github.com>
2024-07-12 19:37:55 -0400

    Delete config directory

--- config/baseDatos.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://<user_name>:<password>@cluster0.6lnoxxb.mongodb.net/my_first_db?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => {
-        console.log("Conexión exitosa a la base de datos 'my_first_db'");
-    })
-    .catch((error) => {
-        console.log(`Hubo un error al conectar con la base de datos: ${error}`);
-    });
